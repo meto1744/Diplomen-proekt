bb9c6624ebe169c780ed3f9aae101173d25301d3
meto1744 <meto17@abv.bg>
2023-02-08 12:20:10 +0200

    rrr

--- WebCocktailBar/WebCocktailBar/Data/ApplicationDbContext.cs
+++ WebCocktailBar/WebCocktailBar/Data/ApplicationDbContext.cs
@@ -3,15 +3,24 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WebCocktailBar.Domain;
 
 namespace WebCocktailBar.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
             Database.EnsureCreated();
         }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Taste> Tastes { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
