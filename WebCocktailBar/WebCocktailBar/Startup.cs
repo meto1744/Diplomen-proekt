15da834aa3bf0df9a95ab28e40b5d072bb1ebb78
meto1744 <meto17@abv.bg>
2023-02-17 10:17:00 +0200

    qqq

--- WebCocktailBar/WebCocktailBar/Startup.cs
+++ WebCocktailBar/WebCocktailBar/Startup.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebCocktailBar.Data;
+using WebCocktailBar.Domain;
 
 namespace WebCocktailBar
 {
@@ -27,13 +28,22 @@ namespace WebCocktailBar
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //options.UseSqlServer(
+            // Configuration.GetConnectionString("DefaultConnection")));
+            //  services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            //  services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            //  .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+              options.UseLazyLoadingProxies()
+              .UseSqlServer(
+              Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
             services.AddControllersWithViews();
 
             services.AddRazorPages();
